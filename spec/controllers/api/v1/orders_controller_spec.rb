63bcc5dec00e386673963e1a997da0d8a2fbcb9d
omarc7 <omarch7@gmail.com>
2017-01-24 10:25:42 +0000

    Adds a custom order product serializer to remove the user association

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -21,13 +21,25 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     before(:each) do
       current_user = FactoryGirl.create :user
       api_authorization_header current_user.auth_token
-      @order = FactoryGirl.create :order, user: current_user
+
+      @product = FactoryGirl.create :product
+      @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
       get :show, params: { user_id: current_user.id, id: @order.id }
     end
 
     it "returns the user order record matching the id" do
       orders_response = json_response
       expect(orders_response[:id]).to eql @order.id
+    end
+
+    it "includes the total for the order" do
+      order_response = json_response
+      expect(order_response[:total]).to eql @order.total.to_s
+    end
+
+    it "includes the products on the order" do
+      order_response = json_response
+      expect(order_response[:products]).to have(1).item
     end
 
     it { expect(response).to have_http_status(200) }
